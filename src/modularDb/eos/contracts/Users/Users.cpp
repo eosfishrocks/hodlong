0fd3b05841152b04241eb35bb56923d6fc26bfa0
Daniel Cowgill <entone@epicdistraction.com>
2018-07-19 20:31:53 -0500

    Updated user contracts to include storage ids

--- src/modularDb/eos/contracts/Users/Users.cpp
+++ src/modularDb/eos/contracts/Users/Users.cpp
@@ -1,85 +1,61 @@
 #include "Users.hpp"
 
-namespace Oasis {
-    using namespace eosio;
-    using std::string;
+namespace hodlong {
+    void Users::add(account_name account, string &username) {
 
-    class Users : public contract {
-        using contract::contract;
+        /**
+         * We require that only the owner of an account can use this action
+         * or somebody with the account authorization
+        */
+        require_auth(account);
 
-    public:
-        Users(account_name self):contract(self) {}
+        /**
+         * We access the "user" table as creating an object of type "userIndex"
+         * As parameters we pass code & scope - _self from the parent contract
+        */
+        userIndex users(_self, _self);
 
-        //@abi action
-        void add(const account_name account, string& username) {
-            /**
-             * We require that only the owner of an account can use this action
-             * or somebody with the account authorization
-            */
-            require_auth(account);
+        /**
+         * We must verify that the account doesn't exist yet
+         * If the account is not found the iterator variable should be users.end()
+        */
+        auto iterator = users.find(account);
+        eosio_assert(iterator == users.end(), "Address for account already exists");
 
-            /**
-             * We access the "user" table as creating an object of type "userIndex"
-             * As parameters we pass code & scope - _self from the parent contract
-            */
-            userIndex users(_self, _self);
+        /**
+         * We add the new user in the table
+         * The first argument is the payer of the storage which will store the data
+        */
+        users.emplace(account, [&](auto &user) {
+            user.account_name = account;
+            user.username = username;
 
-            /**
-             * We must verify that the account doesn't exist yet
-             * If the account is not found the iterator variable should be users.end()
-            */
-            auto iterator = users.find(account);
-            eosio_assert(iterator == users.end(), "Address for account already exists");
+        });
 
-            /**
-             * We add the new user in the table
-             * The first argument is the payer of the storage which will store the data
-            */
-            users.emplace(account, [&](auto& user) {
-                user.account_name = account;
-                user.username = username;
-            });
+    }
+
+    void Users::getuser(const account_name account) {
+        print("Username: ", currentPlayer.username.c_str());
+
+        if (currentPlayer.storageIds.size() > 0) {
+            print(" StorageIds: ");
+
+            for (uint32_t i = 0; i < currentPlayer.abilities.size(); i++) {
+                print(currentPlayer.abilities.at(i).c_str(), " ");
+            }
+        } else {
+            print(" No StorageUsed");
         }
+    }
+    void Users::addstorageId(const accountName, string& storageId){
+        require_auth(account);
 
-        //@abi action
-        void update(account_name account) {
-            require_auth(account);
+        userIndex users(_self, _self);
+        auto iterator = users.find(account);
+        eosio_assert(iterator != users.end(), "Address for account not found");
 
-            userIndex users(_self, _self);
-
-            auto iterator = users.find(account);
-            eosio_assert(iterator != users.end(), "Address for account not found");
+        users.modify(iterator, account, [&](auto& user)) {
+            users.storageIds.push_back(storageId);
         }
-
-        //@abi action
-        void getuser(const account_name account) {
-            userIndex users(_self, _self);
-
-            auto iterator = users.find(account);
-            eosio_assert(iterator != users.end(), "Address for account not found");
-
-            /**
-             * The "get" function returns a constant reference to the object
-             * containing the specified secondary key
-            */
-            auto currentPlayer = users.get(account);
-            print("Username: ", currentPlayer.username.c_str());
-        }
-
-    private:
-
-        //@abi table user i64
-        struct user {
-            uint64_t account_name;
-            string username;
-
-            uint64_t primary_key() const { return account_name; }
-
-            EOSLIB_SERIALIZE(user, (account_name)(username))
-        };
-
-        typedef multi_index<N(user), user> userIndex;
-    };
-
-    EOSIO_ABI(Users, (add)(update)(getuser))
+    }
 }
