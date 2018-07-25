c46cd98737b0270d922568d32fcd1054533b92f5
epicdistraction <daniel@epicdistraction.com>
2018-07-25 15:16:26 -0500

    Added babel-cli and es-2015 preset to compile src

--- __tests__/DownloadManager/DownloadManager.js
+++
@@ -1,6 +0,0 @@
-const { Given, Then, When } = require('cucumber');
-const expect = require('expect');
-const DownloadManger = require('DownloadManager');
-
-Giv
-
---
+++ __tests__/DownloadManager/DownloadManager.steps.js
@@ -0,0 +1,7 @@
+import {loadFeature} from 'jest-cucumber';
+import expect from 'expect';
+import DownloadManger from '../../src/DownloadManager';
+
+
+const feature = loadFeature('./DownloadManager.feature');
+
