1cd4431205ca3514771d3f6ffc38fa2ce7e87735
epicdistraction <daniel@epicdistraction.com>
2018-07-31 23:13:49 -0500

    Added config contract to eos stack to keep track of inital infohashid that contains pubkeys for application

---
+++ src/contracts/Config.cpp
@@ -0,0 +1,16 @@
+#include "Marketplace.hpp"
+
+namespace hodlong {
+    void Config::getConfig() {
+
+    }
+    void Config::changeConfig(account_name admin, string& infoHashID){
+
+    }
+    void Token::getbyid(uint64_t tokenId){
+
+    }
+    void Token::verify(account_name account, uint64_t hashId){
+
+    }
+}
