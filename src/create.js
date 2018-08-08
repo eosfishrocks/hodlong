4d7cc6b0d7e5b850e90f47e449ec4d0823bf923c
epicdistraction <daniel@epicdistraction.com>
2018-08-08 00:35:45 -0500

    Expanded create scripts storage obj

--- src/create.js
+++ src/create.js
@@ -3,7 +3,7 @@ import config from 'config';
 
 
 
-export default function create(input, eos, account, shortname){
+export default function create(input, eos, account, path, short_name){
     const opts = {
         announceList: [[config.TRACKER_ANNOUNCE]],
         comment: `Account: ${account}`,
@@ -15,7 +15,8 @@ export default function create(input, eos, account, shortname){
 
         if (!err) {
             eos.transaction('Users', users => {
-                users.addstorage(account, filename)
+                //#todo calculate checksum
+                users.addstorage(account, `storage{${account},${path},${short_name},null`)
             });
             // return torrent to be added to client for upload to superpeer
             return torrent;
